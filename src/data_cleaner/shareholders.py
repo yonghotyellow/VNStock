359cb78e33e8c4463f452a2272688485aff426a5
yonghotyellow <hieucotre2001@gmail.com>
2025-04-17 11:06:48 +0700

    data standardization for officers and shareholders

--- src/data_cleaner/shareholders.py
+++ src/data_cleaner/shareholders.py
@@ -5,33 +5,33 @@ import pandas as pd
 
 if __name__ == "__main__":
     # Initialize Spark session
-    spark = get_spark("Officer Data")
-    gcs_path = "raw/officers/officers.parquet"
+    spark = get_spark("Shareholder Data", key_path="../../gcs_credentials.json")
+    gcs_path = "raw/shareholders/shareholders.parquet"
 
-    # Load the Parquet file from GCS into a Pandas DataFrame
+    # Load Parquet from GCS into Pandas
     client = get_gcs_client('../../gcs_credentials.json')
     pandas_df = load_parquet_from_gcs(gcs_path, client=client)
 
-    # Convert Pandas DataFrame to Spark DataFrame
+    # Convert Pandas to Spark DataFrame
     spark_df = spark.createDataFrame(pandas_df)
 
-    # Filter out rows where officer_name is "None"
-    df_cleaned = spark_df.filter(spark_df["officer_name"] != "None")
+    # Filter out rows where share_own_percent == 0 OR share_holder == "Khác"
+    df_cleaned = spark_df.filter(
+        (spark_df["share_own_percent"] != 0.0) & (spark_df["share_holder"] != "Khác")
+    )
 
-    # Show counts before and after cleaning
+    # Show counts
     print(f"Original count: {spark_df.count()}")
     print(f"Cleaned count: {df_cleaned.count()}")
     print(f"Removed count: {spark_df.count() - df_cleaned.count()}")
 
-    # Convert the cleaned Spark DataFrame back to Parquet in memory
+    # Convert to Pandas -> Parquet buffer -> Upload
     pdf_cleaned = df_cleaned.toPandas()
     # print(pdf_cleaned.head(50))
     buffer = io.BytesIO()
     pdf_cleaned.to_parquet(buffer, index=False)
     buffer.seek(0)
 
-    # Upload the cleaned Parquet file back to GCS
-    cleaned_gcs_path = "cleaned/officers/officers.parquet"
+    cleaned_gcs_path = "cleaned/shareholders/shareholders.parquet"
     upload_bytes_to_gcs(buffer, cleaned_gcs_path, client=client)
     print(f"Cleaned data uploaded to GCS at {cleaned_gcs_path}")
-
